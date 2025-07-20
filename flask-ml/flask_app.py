df4f600be3c2a287875926702d90316a74e2b4d7
Irfat7 <imirfat10@gmail.com>
2025-07-20 14:25:27 +0600

    preprocessing function in flask api added

--- flask-ml/flask_app.py
+++ flask-ml/flask_app.py
@@ -1,11 +1,26 @@
 from flask import Flask, request, jsonify
 import joblib
+import spacy
 
 app = Flask(__name__)
 
 """ model = joblib.load("model.pkl")
 vectorizer = joblib.load("vectorizer.pkl") """
 
+nlp = spacy.load("en_core_web_sm", disable=["parser", "ner"])
+
+def clean_with_spacy_pipe(text):
+    doc = nlp(text)
+    tokens = [
+        token.lemma_.lower() for token in doc 
+        if not token.is_stop           
+        and not token.is_punct  
+        and token.lemma_ != '-PRON-' 
+        and not token.is_space 
+    ]
+    return " ".join(tokens)
+
 @app.route("/predict", methods=["GET"])
 def predict():
+    # add preprocessing here
     return jsonify({"prediction": "SIU1234"})
