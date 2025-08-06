87fb783784ab51d45a8b0c9ef4c4fc20112bdf57
Irfat7 <imirfat10@gmail.com>
2025-08-06 22:22:19 +0600

    running worker explicitly issue fixed

--- backend/src/queues/combinedWorker.js
+++ backend/src/queues/combinedWorker.js
@@ -1,5 +1,2 @@
-const dbOpsWorker = require("./dbOpsWorker");
-const emailWorker = require("./emailWorker")
-
-dbOpsWorker.run()
-emailWorker.run()
+require("./dbOpsWorker");
+require("./emailWorker")
