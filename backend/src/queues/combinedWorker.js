34792e8f27f4fad33c0c435c5f60faad1c4b800f
Irfat7 <imirfat10@gmail.com>
2025-08-06 23:18:33 +0600

    dummy server added to use web service as background worker

--- backend/src/queues/combinedWorker.js
+++ backend/src/queues/combinedWorker.js
@@ -1,2 +1,11 @@
 require("./dbOpsWorker");
 require("./emailWorker")
+
+// Dummy express server to satisfy Render Web Service
+const express = require('express');
+const app = express();
+
+
+app.listen(PORT, () => {
+  console.log(`Dummy server running`);
+});
