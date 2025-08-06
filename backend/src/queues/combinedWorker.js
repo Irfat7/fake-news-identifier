ea7e84d3730c6fdbf7aac1fb894a50bb60eb1385
Irfat7 <imirfat10@gmail.com>
2025-08-06 23:20:25 +0600

    port issue fixed

--- backend/src/queues/combinedWorker.js
+++ backend/src/queues/combinedWorker.js
@@ -4,8 +4,8 @@ require("./emailWorker")
 // Dummy express server to satisfy Render Web Service
 const express = require('express');
 const app = express();
+const port = 3000
 
-
-app.listen(PORT, () => {
-  console.log(`Dummy server running`);
+app.listen(port, () => {
+    console.log(`Dummy server running`);
 });
